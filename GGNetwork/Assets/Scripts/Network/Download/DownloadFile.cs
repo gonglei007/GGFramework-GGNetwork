7a470033d84843145dfa656f891a78532598f0b0
GL <gonglei@hotmail.com>
2022-12-13 14:58:47 +0800

    整理网络模块； 导入下载功能；

---
+++ GGNetwork/Assets/Scripts/Network/Download/DownloadFile.cs
@@ -0,0 +1,240 @@
+﻿#if UNITY_2017_1_OR_NEWER
+#define DotNet40
+#endif
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using UnityEngine.Networking;
+
+//下载资源文件，支持断点续传
+public class DownlaodFile : MonoBehaviour {
+
+    private static DownlaodFile instance;
+    public static DownlaodFile _Instance
+    {
+        get
+        {
+            if(instance == null)
+            {
+                instance = new GameObject("Download").AddComponent<DownlaodFile>();
+            }
+            return instance;
+        }
+    }
+
+    public class RequestInfo
+    {
+        public string SavePath;
+        public UnityWebRequest WebRequest;
+        public string Url;
+        public Action<float> Progress;
+        public Action<int> TotalLength;
+        public Action<string> Complete;
+
+        public RequestInfo(string savePath, UnityWebRequest request,string url, Action<float> progress, Action<int> totalLength, Action<string> complete)
+        {
+            SavePath = savePath;
+            WebRequest = request;
+            Url = url;
+            Progress = progress;
+            TotalLength = totalLength;
+            Complete = complete;
+        }
+    }
+
+    /// <summary>
+    /// 所有需要下载的资源队列
+    /// </summary>
+    public List<RequestInfo> listRequest = new List<RequestInfo>();    //下载请求的列表
+
+    /// <summary>
+    /// 下载完成后需要清除的Url列表
+    /// </summary>
+    List<RequestInfo> removeList = new List<RequestInfo>();
+
+    /// <summary>
+    /// 下载单个文件的时间
+    /// </summary>
+    private float DownloadTime = 0;
+
+    private bool IsStartDownLoad = false;
+
+    public void ClearData()
+    {
+        foreach (RequestInfo requestInfo in listRequest)
+        {
+            RequestDispose(requestInfo, true);
+        }
+        listRequest.Clear();
+        removeList.Clear();
+        DownloadTime = 0;
+        IsStartDownLoad = false;
+    }
+    private bool IsInlistRequest(string url)
+    {
+        for (int i = 0; i < listRequest.Count; i++)
+        {
+            if (listRequest[i].Url == url)
+                return true;
+        }
+
+        return false;
+    }
+
+    public bool IsDownLoadLimit
+    {
+        get
+        {
+            return listRequest.Count >= 20;
+        }
+    }
+
+    /// <summary>
+    /// 下载文件
+    /// </summary>
+    /// <param name="url"></param>
+    /// <param name="savePath"></param>
+    /// <param name="progress"></param>
+    /// <param name="TotalLength"></param>
+    /// <param name="complete"></param>
+    public  void StartDownLoad(string url,string savePath, Action<float> progress, Action<int> TotalLength, Action<string> complete)
+    {
+        if (IsDownLoadLimit)
+            return;
+
+        IsStartDownLoad = true;
+
+        if (IsInlistRequest(url))
+        {
+            Debug.Log("下载列表已经存在路径=>" + url);
+            return;
+        }
+
+        if (File.Exists(savePath))
+        {
+            File.Delete(savePath);
+        }
+        string targetFilePath = Path.GetDirectoryName(savePath);
+        if (!Directory.Exists(targetFilePath))
+            Directory.CreateDirectory(targetFilePath);
+
+        RequestInfo requestInfo = new RequestInfo(savePath, null, url, progress, TotalLength, complete);
+
+        Send(requestInfo);
+
+        listRequest.Add(requestInfo);  
+    }
+
+    private void Send(RequestInfo requestInfo)
+    {
+        _DownloadHandler loadHandler = new _DownloadHandler(requestInfo.SavePath);
+        loadHandler.RegisteProgressBack(requestInfo.Progress);
+        loadHandler.RegisteReceiveTotalLengthBack(requestInfo.TotalLength);
+        loadHandler.RegisteCompleteBack(requestInfo.Complete);
+        UnityWebRequest request = UnityWebRequest.Get(requestInfo.Url);
+        request.timeout = 25;
+        request.chunkedTransfer = true;
+        request.disposeDownloadHandlerOnDispose = true;
+        request.SetRequestHeader("Range", "bytes=" + loadHandler.DownedLength + "-");
+        request.downloadHandler = loadHandler;
+        request.Send();
+        requestInfo.WebRequest = request;
+    }
+
+    private void Update()
+    {
+        if (!IsStartDownLoad)
+            return;
+
+        if (DownloadTime < 30)
+        {
+            DownloadTime += Time.deltaTime;
+        }
+        else
+        {
+            //TODO:GL - 使用新的UI系统！
+            /*
+            GameLauncher.ShowDialog
+               (
+                       TextManager.GetText("network_error"),
+                       null,
+                       ConfirmStyle.YesNo, XiaoChanStyle.normal,
+                       (bool ret) =>
+                       {
+                           if (ret)
+                               DownloadTime = 0;
+                           else
+                               Application.Quit();
+                       },
+                       null,
+                       TextManager.GetText("retry"),
+                       TextManager.GetText("quit"),
+                       false
+              );
+              */
+        }
+
+        for (int i = 0; i < listRequest.Count; i++)
+        {
+            UnityWebRequest request = listRequest[i].WebRequest;
+#if DotNet40
+            bool result = request.isNetworkError;
+#else
+            bool result = request.isError;
+#endif
+
+            if (request!= null && result)
+            {
+                if (GameDebugger.Instance != null)
+                    GameDebugger.Instance.PushLog(string.Format("下载出错:{0},准备重试下载", request.error));
+                RequestDispose(listRequest[i], false);
+                Send(listRequest[i]);
+                continue;
+            }
+            if (request!=null && request.isDone)
+            {
+                removeList.Add(listRequest[i]);
+                RequestDispose(listRequest[i], true);
+                DownloadTime = 0;
+            }
+        }
+
+        for (int i = 0; i < removeList.Count; i++)
+        {
+            if(listRequest.Contains(removeList[i]))
+                listRequest.Remove(removeList[i]);
+        }
+        removeList.Clear();
+    }
+
+    void OnApplicationQuit()
+    {
+        ClearData();
+    }
+
+    /// <summary>
+    /// 释放下载请求
+    /// </summary>
+    /// <param name="requestInfo"></param>
+    private void RequestDispose(RequestInfo requestInfo,bool isClearSelf)
+    {
+        if (requestInfo == null || requestInfo.WebRequest == null)
+            return;
+
+        (requestInfo.WebRequest.downloadHandler as _DownloadHandler).OnDispose();  //释放资源
+        requestInfo.WebRequest.Dispose();
+        requestInfo.WebRequest = null;
+
+        if (isClearSelf)
+        {
+            requestInfo.Progress = null;
+            requestInfo.Complete = null;
+            requestInfo.TotalLength = null;
+            requestInfo.Url = null;
+            requestInfo.SavePath = null;
+            requestInfo = null;
+        }
+    }
+}
---
+++ GGNetwork/Assets/Scripts/Network/Download/_DownloadHandler.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using UnityEngine.Networking;
+//using GOLib.Tool;
+
+//下载句柄，自定义部分操作和数据
+public class _DownloadHandler : DownloadHandlerScript
+{
+    //文件流，用来写入文件数据
+    private FileStream fs;  
+
+    //要下载的文件总长度
+    private int _contentLength = 0;
+    public int ContentLength
+    {
+        get { return _contentLength; }
+    }
+
+    //已经下载的数据长度
+    private int _downedLength = 0;   
+    public int DownedLength
+    {
+        get { return _downedLength; }
+    }
+
+    //要保存的文件名称，带扩展名
+    private string _fileName;    
+    public string FileName
+    {
+        get { return _fileName; }
+    }
+
+    //下载中的临时文件名，可自定义：fileName+.temp
+    public string FileNameTemp
+    {
+        get { return _fileName + ".temp"; }
+    }
+
+    //要保存的文件路径
+    private string savePath = null;
+    //保存的文件目录名称
+    public string DirectoryPath
+    {
+        get { return savePath.Substring(0, savePath.LastIndexOf('/')); }
+    }
+
+    #region 消息通知事件
+
+    private event Action<int> _eventTotalLength = null; //接收到数据总长度的事件回调，传递的参数是文件总大小，单位：字节
+
+    private event Action<float> _eventProgress = null;    //进度通知事件，传递的是进度浮点数
+
+    private event Action<string> _eventComplete = null;  //完成后的事件回调，传递的参数是文件路径
+
+    #endregion
+
+    #region 注册消息事件
+
+    //注册收到文件总长度的事件，传递的参数是文件总大小，单位：字节
+    public void RegisteReceiveTotalLengthBack(Action<int> back)
+    {
+        if (back != null)
+            _eventTotalLength = back;
+    }
+
+    //注册进度事件，传递的是进度浮点数
+    public void RegisteProgressBack(Action<float> back)
+    {
+        if (back != null)
+            _eventProgress = back;
+    }
+
+    //注册下载完成后的事件，传递的参数是文件路径
+    public void RegisteCompleteBack(Action<string> back)
+    {
+        if (back != null)
+            _eventComplete = back;
+    }
+
+    #endregion
+
+    /// <summary>
+    /// 初始化下载句柄，定义每次下载的数据上限为200kb
+    /// </summary>
+    /// <param name="filePath">保存到本地的文件路径</param>
+    public _DownloadHandler(string filePath):base(new byte[1024*200])
+    {
+        savePath = filePath.Replace('\\','/');
+        _fileName = savePath.Substring(savePath.LastIndexOf('/') + 1);  //获取文件名
+
+         this.fs = new FileStream(savePath + ".temp", FileMode.Append, FileAccess.Write);    //文件流操作的是临时文件，结尾添加.temp扩展名
+         _downedLength = (int)fs.Length;  //设置已经下载的数据长度
+    }
+
+    /// <summary>
+    /// 当从网络接收数据时的回调，每帧调用一次
+    /// </summary>
+    /// <param name="data">接收到的数据字节流，总长度为构造函数定义的200kb，并非所有的数据都是新的</param>
+    /// <param name="dataLength">接收到的数据长度，表示data字节流数组中有多少数据是新接收到的，即0-dataLength之间的数据是刚接收到的</param>
+    /// <returns>返回true表示当下载正在进行，返回false表示下载中止</returns>
+    protected override bool ReceiveData(byte[] data, int dataLength)
+    {
+        if (data == null || data.Length == 0)
+        {
+            Debug.Log("没有获取到数据缓存!");
+            return false;
+        }
+
+        fs.Write(data, 0, dataLength);
+        _downedLength += dataLength;
+
+        if (_eventProgress != null)
+            _eventProgress.Invoke((float)_downedLength / _contentLength);   //通知进度消息
+
+        return true;
+    }
+
+	public IEnumerator MoveArchieveZipFile(string TempFilePath, string CompleteFilePath)
+	{
+		yield return new WaitForSeconds(0.2f);
+		File.Move(TempFilePath, CompleteFilePath);
+
+		if (_eventComplete != null)
+			_eventComplete.Invoke(CompleteFilePath);
+	}
+
+    public void OnDispose()
+    {
+        if (fs != null)
+        {
+            fs.Close();
+            fs.Dispose();
+        }
+    }
+
+    /// <summary>
+    /// 请求下载时，会先接收到文件的数据总量
+    /// </summary>
+    /// <param name="contentLength">如果是从网络上下载资源，则表示文件剩余下载的大小；如果是本地拷贝资源，则表示文件总长度</param>
+    protected override void ReceiveContentLength(int contentLength)
+    {
+        _contentLength = contentLength + _downedLength;
+        if (_eventTotalLength != null)
+            _eventTotalLength.Invoke(_contentLength);
+    }
+}
